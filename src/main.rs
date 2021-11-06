3ca427836b20dd75df07e012ddade5d7fdde0a6b
Alan Falloon <alan.falloon@gmail.com>
2021-11-06 00:33:10 -0400

    Try to make macroquad to play nice with wasm-pack
    Tried using `trunk` to serve the files, but `macroquad` doesn't like
    being loaded that way.
    
    When I use the hard-coded, unpacked wasm like in the `html/` dir, then
    it works as long as I don't need any `wasm-bindgen` symbols. As soon as
    I enable `console_error_panic_hook` for example, the bare WASM will no
    longer load because of missing `wasm-bindgen` symbols.
    
    Tried installing `wasm-pack` to generate a `--target=web` bundle, but it
    causes the linker to abort. I'm going to update my toolchain and try
    again tomorrow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use macroquad::prelude::*;
+#[macroquad::main("BumbleUmbleGee")]
+async fn main() {
+    // #[cfg(target_arch = "wasm32")]
+    // std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    loop {
+        clear_background(LIGHTGRAY);
+        let middle = (screen_width() / 2., screen_height() / 2.);
+        draw_circle(middle.0, middle.1, 60., BLACK);
+        next_frame().await
+    }
 }
